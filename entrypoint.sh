5e267bae9dcd1e9a8899eaacaab25b2eed3d1c4f
Varun <varunsridharan23@gmail.com>
2020-06-27 15:33:39 +0530

    Improved log

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ echo "###[group] 🛠️  Setting Required ENV Variables"
 bash /vs-action-utility/setup-env.sh
 echo "###[endgroup]"
 
+echo " "
+
 echo "###[group] Handle Envato Workflow"
 bash /vs-action-utility/envato/ignore.sh
 echo "###[endgroup]"
