04088423be08ce7024e09982fda86d15e806cafc
Varun <varunsridharan23@gmail.com>
2020-06-28 17:58:27 +0530

    Updated

--- app/functions.php
+++ app/functions.php
@@ -8,8 +8,7 @@
  */
 function vs_set_action_env( $key, $value ) {
 	$output = '';
-	//exec( , $output );
-	var_dump( shell_exec( 'sh ' . APP_PATH . "set-env.sh $key $value" ) );
+	var_dump( shell_exec( 'echo "::set-env name=' . $key . '::' . $value . '"' ) );
 	//var_dump( shell_exec( "echo \"::set-env name=$key::$value\"" ) );
 	//var_dump( $output );
 }
