834028a23d8cd4cd7a02a1032cb111b6890485a4
Varun <varunsridharan23@gmail.com>
2020-06-28 18:15:34 +0530

    updated

--- app/set-env-vars.php
+++ app/set-env-vars.php
@@ -1,4 +1,16 @@
 <?php
+echo '###[group] 🛠️  Setting Required ENV Variables';
+
+echo 'Setting UP : GH_LOCAL_KEY';
 var_dump( vs_set_action_evn_ifnot_exists( 'GH_LOCAL_KEY', 'Here Is Your KEY 1' ) );
+
+echo 'Setting UP : GH_LOCAL_KEYE';
 var_dump( vs_set_action_evn_ifnot_exists( 'GH_LOCAL_KEYE', 'Here Is Your KEY 2' ) );
+
+echo " ";
+
+echo 'Getting : GH_LOCAL_KEY';
 var_dump( getenv( 'GH_LOCAL_KEY ' ) );
+
+
+echo '###[endgroup]';
