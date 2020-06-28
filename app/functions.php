697d17cef926f26af4abf32c47a9c66f68c053de
Varun <varunsridharan23@gmail.com>
2020-06-28 19:41:54 +0530

    Updated SETENV

--- app/functions.php
+++ app/functions.php
@@ -4,6 +4,7 @@
  *
  * @param $key
  * @param $value
+ * @param $msg
  *
  * @return true
  */
@@ -18,26 +19,50 @@ function set_action_env( $key, $value ) {
  *
  * @param $key
  * @param $value
+ * @param $msg
  *
  * @return bool
  */
-function set_action_env_not_exists( $key, $value ) {
-	return ( ! isset( $_ENV[ $key ] ) ) ? set_action_env( $key, $value ) : false;
+function set_action_env_not_exists( $key, $value, $msg = false ) {
+	if ( ! isset( $_ENV[ $key ] ) ) {
+		set_action_env( $key, $value );
+		if ( $msg ) {
+			_echo( "✔️ ENV  ${key} SET WITH VALUE ${value}" );
+		}
+		return true;
+	}
+	_echo( "ℹ️ ENV ${key} ALREADY EXISTS WITH VALUE - {$_ENV[$key]}" );
+	return false;
 }
 
-function get_env( $key ) {
-	return ( isset( $_ENV[ $key ] ) ) ? $_ENV[ $key ] : new ENV_Not_Exists();
+/**
+ * @param      $key
+ * @param null $default
+ *
+ * @return \ENV_Not_Exists|mixed
+ */
+function get_env( $key, $default = null ) {
+	$default = ( null === $default ) ? new ENV_Not_Exists() : $default;
+	return ( isset( $_ENV[ $key ] ) ) ? $_ENV[ $key ] : $default;
 }
 
+/**
+ * @param $instance
+ *
+ * @return bool
+ */
 function is_env_not_exists( $instance ) {
 	return ( $instance instanceof \ENV_Not_Exists );
 }
 
+/**
+ * @param      $content
+ * @param bool $is_cmd
+ */
 function _echo( $content, $is_cmd = false ) {
 	if ( $is_cmd ) {
 		echo PHP_EOL . $content . PHP_EOL;
 	} else {
 		echo $content . PHP_EOL;
 	}
-
 }
--- app/set-env-vars.php
+++ app/set-env-vars.php
@@ -1,15 +1,33 @@
 <?php
 _echo( '###[group] 🛠️  Setting Required ENV Variables' );
 
-var_dump( set_action_env_not_exists( 'GH_LOCAL_KEY', 'Here Is Your KEY 1' ) );
-var_dump( set_action_env_not_exists( 'GH_LOCAL_KEYE', 'Here Is Your KEY 2' ) );
+$github_repo = get_env( 'GITHUB_REPOSITORY', false );
+$github_repo = explode( '/', $github_repo );
+$owner       = '';
+$repo_slug   = '';
 
-_echo( '' );
+if ( ! empty( $github_repo ) && is_array( $github_repo ) ) {
+	$owner     = isset( $github_repo[0] ) ? $github_repo[0] : '';
+	$repo_slug = isset( $github_repo[1] ) ? $github_repo[1] : '';
+}
 
-_echo( 'Getting : GH_LOCAL_KEY' );
+$env_vars                                 = array();
+$workspace                                = get_env( 'GITHUB_WORKSPACE', false );
+$sha_short                                = substr( get_env( 'GITHUB_SHA', false ), 0, 8 );
+$env_vars['GITHUB_REPOSITORY_OWNER']      = trim( $owner );
+$env_vars['GITHUB_REPOSITORY_SLUG']       = trim( $repo_slug );
+$env_vars['GITHUB_SHA_SHORT']             = $sha_short;
+$env_vars['VS_BEFORE_HOOK_FILE']          = 'before.sh';
+$env_vars['VS_AFTER_HOOK_FILE']           = 'after.sh';
+$env_vars['VS_BEFORE_HOOK_FILE_LOCATION'] = "${workspace}/.github/workspace/{$env_vars['VS_BEFORE_HOOK_FILE']}";
+$env_vars['VS_AFTER_HOOK_FILE_LOCATION']  = "${workspace}/.github/workspace/{$env_vars['VS_AFTER_HOOK_FILE']}";
+$env_vars['CHLOG_REPO_ORG_NAME']          = 'vs-docs';
+$env_vars['LOCAL_CHANGE_LOG_FILE']        = 'CHANGELOG.md';
+$env_vars['REMOTE_CHANGE_LOG_FILE']       = 'change-log.md';
 
-var_dump( get_env( 'GH_LOCAL_KEY' ) );
 
-_echo( '' );
+foreach ( $env_vars as $id => $val ) {
+	set_action_env_not_exists( $id, $val, true );
+}
 
 _echo( '###[endgroup]' );
