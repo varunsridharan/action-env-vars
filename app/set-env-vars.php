e3a1330b0fd87ad2c64ac64521f5bcf2b26f66af
Varun <varunsridharan23@gmail.com>
2020-06-28 18:06:08 +0530

    Removed unwanted File

--- app/set-env-vars.php
+++ app/set-env-vars.php
@@ -2,4 +2,4 @@
 
 var_dump( vs_set_action_evn_ifnot_exists( 'GH_LOCAL_KEY', 'Here Is Your KEY 1' ) );
 var_dump( vs_set_action_evn_ifnot_exists( 'GH_LOCAL_KEYE', 'Here Is Your KEY 2' ) );
-var_dump( getenv( 'GH_LOCAL_KEY' ) );
+var_dump( getenv( 'GH_LOCAL_KEY ' ) );
--- app/set-env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-echo "::set-env name=$1::$2"
