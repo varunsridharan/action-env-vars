2aea6f79c252ccefb6ea1f9baa53f0e71559c924
Varun <varunsridharan23@gmail.com>
2020-06-28 18:23:02 +0530

    m

--- app/app.php
+++ app/app.php
@@ -1,10 +1,8 @@
 <?php
 define( 'APP_PATH', __DIR__ . '/' );
 
+var_dump(11111111111111111111111111111111);
 require_once APP_PATH . 'functions.php';
-
-
-
 require_once APP_PATH . 'set-env-vars.php';
 
 
