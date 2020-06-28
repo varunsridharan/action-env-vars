f72e3035311e4936c282afa64eaa764e53d0f244
Varun <varunsridharan23@gmail.com>
2020-06-28 21:53:46 +0530

    Updated

--- app/app.php
+++ app/app.php
@@ -7,6 +7,7 @@ require_once APP_PATH . 'class-env-not-eixsts.php';
 
 require_once APP_PATH . 'set-env-vars.php';
 
+require_once APP_PATH . 'repo-title.php';
+
 require_once APP_PATH . 'envato.php';
 
-require_once APP_PATH . 'repo-title.php';
