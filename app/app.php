09fc96eeb0f4e15c43068fc39ce3aded93691ddb
Varun <varunsridharan23@gmail.com>
2020-06-28 18:24:43 +0530

    Only GET

--- app/app.php
+++ app/app.php
@@ -1,8 +1,6 @@
 <?php
 define( 'APP_PATH', __DIR__ . '/' );
 
-var_dump(11111111111111111111111111111111);
 require_once APP_PATH . 'functions.php';
+var_dump( 11111111111111111111111111111111 );
 require_once APP_PATH . 'set-env-vars.php';
-
-
--- app/set-env-vars.php
+++ app/set-env-vars.php
@@ -1,8 +1,8 @@
 <?php
 echo '###[group] 🛠️  Setting Required ENV Variables' . PHP_EOL;
 
-var_dump( vs_set_action_evn_ifnot_exists( 'GH_LOCAL_KEY', 'Here Is Your KEY 1' ) );
-var_dump( vs_set_action_evn_ifnot_exists( 'GH_LOCAL_KEYE', 'Here Is Your KEY 2' ) );
+#var_dump( vs_set_action_evn_ifnot_exists( 'GH_LOCAL_KEY', 'Here Is Your KEY 1' ) );
+#var_dump( vs_set_action_evn_ifnot_exists( 'GH_LOCAL_KEYE', 'Here Is Your KEY 2' ) );
 
 echo " ";
 
