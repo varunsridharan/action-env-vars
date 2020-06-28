ae82a1ddd8d2c82b51bafd6d422822ad0cedaf1c
Varun <varunsridharan23@gmail.com>
2020-06-28 21:54:27 +0530

    Updated

--- app/app.php
+++ app/app.php
@@ -1,13 +1,17 @@
 <?php
 define( 'APP_PATH', __DIR__ . '/' );
 
-require_once APP_PATH . 'functions.php';
+try {
+	require_once APP_PATH . 'functions.php';
 
-require_once APP_PATH . 'class-env-not-eixsts.php';
+	require_once APP_PATH . 'class-env-not-eixsts.php';
 
-require_once APP_PATH . 'set-env-vars.php';
+	require_once APP_PATH . 'set-env-vars.php';
 
-require_once APP_PATH . 'repo-title.php';
+	require_once APP_PATH . 'repo-title.php';
 
-require_once APP_PATH . 'envato.php';
+	require_once APP_PATH . 'envato.php';
 
+} catch ( Exception $exception ) {
+	die( '::error:: ' . $exception->getMessage() );
+}
