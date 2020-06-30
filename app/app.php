b1f254fec82df25ab57816a024c64c68535ba88f
Varun <varunsridharan23@gmail.com>
2020-06-30 12:27:38 +0530

    Removed Workflow SYNC

--- app/app.php
+++ app/app.php
@@ -9,21 +9,13 @@ try {
 
 	$workflow_type = ( is_env_not_exists( get_env( 'VS_WORKFLOW_TYPE' ) ) ) ? false : get_env( 'VS_WORKFLOW_TYPE' );
 
-	set_action_env_not_exists( 'MY_LARGE_VAR', ' | VALUE1 VALUE2', true );
-	exit;
+	require_once APP_PATH . 'set-env-vars.php';
 
-	if ( in_array( $workflow_type, array( 'workflow-sync', 'secrets-sync' ) ) ) {
-		require_once APP_PATH . 'workflow-secrets-sync.php';
-	} else {
-		require_once APP_PATH . 'set-env-vars.php';
+	require_once APP_PATH . 'repo-title.php';
 
-		require_once APP_PATH . 'repo-title.php';
-
-		if ( 'envato-release' === $workflow_type ) {
-			require_once APP_PATH . 'envato.php';
-		}
+	if ( 'envato-release' === $workflow_type ) {
+		require_once APP_PATH . 'envato.php';
 	}
-
 } catch ( Exception $exception ) {
 	die( '::error:: ' . $exception->getMessage() );
 }
