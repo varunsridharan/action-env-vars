f1a90f966c3f8344769ab1b9d4de16e2aebfabc8
Varun <varunsridharan23@gmail.com>
2020-07-01 09:02:40 +0530

    Removed Scripts.

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,9 @@ LABEL maintainer="Varun Sridharan <varunsridharan23@gmail.com>"
 
 COPY entrypoint.sh /entrypoint.sh
 
-COPY scripts /vs-action-utility/
-
 COPY app /vs-utility-app/
 
 RUN chmod 777 entrypoint.sh
-
-RUN chmod -R 777 /vs-action-utility/
 
 RUN chmod -R 777 /vs-utility-app/
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,3 @@
 #!/bin/sh
-
 echo " "
-
 php /vs-utility-app/app.php
-
-#echo "###[group] 🛠️  Setting Required ENV Variables"
-#bash /vs-action-utility/setup-env.sh
-#echo "###[endgroup]"
-#
-#echo " "
-#
-#echo "###[group] Setup Repository Title"
-#bash /vs-action-utility/repo-title.sh
-#echo "###[endgroup]"
-#
-#echo " "
-#
-#echo "###[group] Handle Envato Workflow"
-#bash /vs-action-utility/envato/ignore.sh
-#echo "###[endgroup]"
-#
-#echo " "
