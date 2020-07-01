bb2850300b261846b47ce9c2073c83940cc2ecb7
Varun <varunsridharan23@gmail.com>
2020-07-01 09:08:09 +0530

    Improved Log

--- app/envato/ignore.php
+++ app/envato/ignore.php
@@ -5,4 +5,5 @@ require_once APP_PATH . 'class/class-ignore-envato.php';
 $ignore = new Envato_Ignore();
 
 set_action_env_not_exists( 'ENVATO_DIST_IGNORE', $ignore->run( 'ignore' ), true );
+_echo( '----' );
 set_action_env_not_exists( 'ENVATO_ASSETS_DIST_IGNORE', $ignore->run( 'assets' ), true );
