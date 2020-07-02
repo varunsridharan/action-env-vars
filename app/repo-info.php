bd9c2646aab2c1c7766832216d3efe82762dc6fa
Varun <varunsridharan23@gmail.com>
2020-07-02 13:29:40 +0530

    Migrated Some VArs To Its Own Github Action https://github.com/marketplace/actions/github-repository-metadata

--- app/repo-info.php
+++ app/repo-info.php
@@ -3,10 +3,4 @@ _echo( '###[group] 🔧  Setup Repository Information' );
 
 require_once APP_PATH . 'repo-info/title.php';
 
-if ( ! is_env_not_exists( get_env( 'GITHUB_TOKEN' ) ) ) {
-	require_once APP_PATH . 'vendor/autoload.php';
-	require_once APP_PATH . 'repo-info/type.php';
-}
-
-
 _echo( '###[endgroup]' );
--- app/set-env-vars.php
+++ app/set-env-vars.php
@@ -2,22 +2,8 @@
 
 _echo( '###[group] 🛠️  Setting Required ENV Variables' );
 
-$github_repo = get_env( 'GITHUB_REPOSITORY', false );
-$github_repo = explode( '/', $github_repo );
-$owner       = '';
-$repo_slug   = '';
-
-if ( ! empty( $github_repo ) && is_array( $github_repo ) ) {
-	$owner     = isset( $github_repo[0] ) ? $github_repo[0] : '';
-	$repo_slug = isset( $github_repo[1] ) ? $github_repo[1] : '';
-}
-
 $env_vars                                 = array();
 $workspace                                = get_env( 'GITHUB_WORKSPACE', false );
-$sha_short                                = substr( get_env( 'GITHUB_SHA', false ), 0, 8 );
-$env_vars['GITHUB_REPOSITORY_OWNER']      = trim( $owner );
-$env_vars['GITHUB_REPOSITORY_SLUG']       = trim( $repo_slug );
-$env_vars['GITHUB_SHA_SHORT']             = $sha_short;
 $env_vars['VS_BEFORE_HOOK_FILE']          = 'before.sh';
 $env_vars['VS_AFTER_HOOK_FILE']           = 'after.sh';
 $env_vars['VS_BEFORE_HOOK_FILE_LOCATION'] = "${workspace}/.github/workspace/{$env_vars['VS_BEFORE_HOOK_FILE']}";
@@ -25,7 +11,6 @@ $env_vars['VS_AFTER_HOOK_FILE_LOCATION']  = "${workspace}/.github/workspace/{$en
 $env_vars['CHLOG_REPO_ORG_NAME']          = 'vs-docs';
 $env_vars['LOCAL_CHANGE_LOG_FILE']        = 'CHANGELOG.md';
 $env_vars['REMOTE_CHANGE_LOG_FILE']       = 'change-log.md';
-
 
 foreach ( $env_vars as $id => $val ) {
 	set_action_env_not_exists( $id, $val, true );
