b41b33aff95d72f3e0a22bfa66c52b8614905961
Varun <varunsridharan23@gmail.com>
2020-07-02 08:30:45 +0530

    Added API DATA

--- app/repo-info/type.php
+++ app/repo-info/type.php
@@ -11,5 +11,8 @@ use Milo\Github\OAuth\Token;
 
 $gh_api = new Api();
 $gh_api->setToken( new Token( $token ) );
-
-var_dump( $gh_api->decode( $gh_api->get( 'repos/' . $repo ) ) );
+$data = $gh_api->decode( $gh_api->get( 'repos/' . $repo ) );
+_echo( ' ' );
+_echo( json_encode( $data ) );
+_echo( '' );
+_echo( var_export( $data, true ) );
