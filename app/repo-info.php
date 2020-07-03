8295f9019956a4ef33b29fe8a8aae1b2e2f8b985
Varun <varunsridharan23@gmail.com>
2020-07-03 06:50:16 +0530

    Added Option to extract README From Github README.md

--- app/repo-info.php
+++ app/repo-info.php
@@ -1,6 +1,7 @@
 <?php
 _echo( '###[group] 🔧  Setup Repository Information' );
 
+require_once APP_PATH . 'class/slim-markdown.php';
 require_once APP_PATH . 'repo-info/title.php';
 
 _echo( '###[endgroup]' );
--- app/repo-info/title.php
+++ app/repo-info/title.php
@@ -1,26 +1,60 @@
 <?php
 _echo( '' );
+$repo_title = get_env( 'REPOSITORY_NAME' );
+$slug       = get_env( 'REPOSITORY_SLUG' );
+$workspace  = get_env( 'GITHUB_WORKSPACE' );
+$title      = '';
+$rmf        = false;
 
-$repo_title = get_env( 'GITHUB_REPOSITORY_TITLE' );
-if ( ! is_env_not_exists( $repo_title ) || ! empty( $repo_title ) ) {
-	if ( ! is_env_not_exists( get_env( 'REPOSITORY_SLUG' ) ) ) {
-		$slug        = get_env( 'REPOSITORY_SLUG' );
+if ( ! is_env_not_exists( $slug ) ) {
+
+	if ( file_exists( $workspace . '/readme.md' ) ) {
+		$rmf = 'readme.md';
+	}
+
+	if ( file_exists( $workspace . '/README.md' ) ) {
+		$rmf = 'README.md';
+	}
+
+	if ( file_exists( $workspace . '/readme.MD' ) ) {
+		$rmf = 'readme.MD';
+	}
+
+	if ( file_exists( $workspace . '/README.MD' ) ) {
+		$rmf = 'README.MD';
+	}
+
+	if ( ! empty( $rmf ) ) {
+		_echo( '🎞️ README.md Found - Extracting Title' );
+		$rmf = file_get_contents( $rmf );
+		preg_match( '/^(#\s)(.*)/m', $rmf, $matches, 0, 0 );
+		if ( isset( $matches[2] ) ) {
+			$matches[2] = strip_tags( Slimdown::render( $matches[2] ) );
+			if ( ! empty( $matches[2] ) ) {
+				$title = $matches[2];
+				_echo( '✔️ Repository Title Found In README.md' );
+			}
+		}
+	}
+
+	if ( empty( $title ) ) {
 		$repo_titles = json_decode( file_get_contents( 'https://cdn.svarun.dev/json/repo-titles.json' ), true );
-		$title       = '';
 		_echo( '🎞️ Checking - Repository Title Database' );
 
 		if ( isset( $repo_titles[ $slug ] ) ) {
 			$title = $repo_titles[ $slug ];
 			_echo( '✔️ Repository Title Found In Database' );
-		} else {
-			$title = ucwords( str_replace( '-', ' ', trim( $slug ) ) );
-			_echo( '⚠️ Repository Title Not Found In Database | Using Slug is used as Title' );
 		}
-		set_action_env_not_exists( 'GITHUB_REPOSITORY_TITLE', $title, true );
-	} else {
-		_echo( '🛑 Repository SLUG Not Found !' );
 	}
+
+	if ( empty( $title ) ) {
+		$title = $repo_title;
+		_echo( '⚠️ Repository Title Not Found In Database | Using Slug is used as Title' );
+	}
+
+	set_action_env_not_exists( 'GITHUB_REPOSITORY_TITLE', $title, true );
 } else {
-	_echo( "✔️ Repository Title Already Set - ${repo_title}" );
+	_echo( '🛑 Repository SLUG Not Found !' );
 }
+
 _echo( '' );
