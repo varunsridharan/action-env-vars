1cd9fade5c402bc95c0e927277d7d21d1b73b203
Varun <varunsridharan23@gmail.com>
2020-07-04 06:55:32 +0530

    Added REPO TYPE

--- app/repo-info.php
+++ app/repo-info.php
@@ -3,5 +3,6 @@ _echo( '###[group] 🔧  Setup Repository Information' );
 
 require_once APP_PATH . 'class/slim-markdown.php';
 require_once APP_PATH . 'repo-info/title.php';
+require_once APP_PATH . 'repo-info/type.php';
 
 _echo( '###[endgroup]' );
