099cae626def891ded35e9d9fd972ae83e92f4b7
Varun <varunsridharan23@gmail.com>
2020-07-04 20:35:46 +0530

    Updated Twitter

--- app/twitter.php
+++ app/twitter.php
@@ -19,19 +19,27 @@ if ( ! empty( $topics ) ) {
 	$topics = json_decode( $topics, true );
 }
 
-if ( in_array( 'vs-envato-plugin', $topics ) ) {
-
+if ( in_array( 'css-library', $topics ) || in_array( 'vs-css-library', $topics ) ) {
 }
 
-if ( in_array( 'vs-wp-plugin', $topics ) ) {
-
+if ( in_array( 'javascript-library', $topics ) || in_array( 'vs-javascript-library', $topics ) ) {
 }
 
-if ( in_array( 'wp-library', $topics ) ) {
-
+if ( in_array( 'php-library', $topics ) || in_array( 'vs-php-library', $topics ) ) {
 }
 
-if ( in_array( 'php-library', $topics ) ) {
+if ( in_array( 'wp-library', $topics ) || in_array( 'vs-wp-library', $topics ) ) {
+}
+
+if ( in_array( 'wp-plugin', $topics ) || in_array( 'vs-wp-plugin', $topics ) ) {
+}
+
+if ( in_array( 'envato-plugin', $topics ) || in_array( 'vs-envato-plugin', $topics ) ) {
+}
+
+if ( in_array( 'github-action', $topics ) || in_array( 'vs-github-action', $topics ) ) {
+	$message = "📢 ${repo_title} V ${release_tag_name} Released 🎉 ${homeurl}
+	#github #githubaction #actions";
 }
 
 if ( empty( $message ) ) {
