bb2af645873283aa7d8c1eaf0f044f5e7bbfcf6e
Varun <varunsridharan23@gmail.com>
2020-09-28 17:02:17 +0530

    Updated!

--- app/workflows/github-package-registry-npm-release.php
+++ app/workflows/github-package-registry-npm-release.php
@@ -5,13 +5,14 @@ if ( 'github-package-registry-npm-release' === WORKFLOW_TYPE ) {
 		$contents = json_decode( $contents, true );
 		print_r( $contents );
 
-		if ( preg_match( '#^' . '@varunsridharan' . '#', $contents['name'] ) === 1 ) {
-
-		} else {
+		if ( ! ( preg_match( '#^' . '@varunsridharan' . '#', $contents['name'] ) === 1 ) ) {
 			$contents['name'] = '@' . REPO_OWNER . '/' . $contents['name'];
 		}
+		$contents['publishConfig'] = array( 'registry' => 'https://npm.pkg.github.com/' );
 		print_r( $contents );
-
+		file_put_contents( repo_file( 'package.json' ), json_encode( $contents, JSON_PRETTY_PRINT ) );
+		$contents = file_get_contents( repo_file( 'package.json' ) );
+		echo $contents;
 	} else {
 		__( '🛑 Unable To Find Package.json' );
 	}
