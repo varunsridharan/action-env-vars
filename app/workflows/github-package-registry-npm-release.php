c3b959d4f23d666dc7dc39e709aaf04f1a327d05
Varun <varunsridharan23@gmail.com>
2020-09-28 16:37:10 +0530

    WIP for Workflow.

---
+++ app/workflows/github-package-registry-npm-release.php
@@ -0,0 +1,18 @@
+<?php
+if ( 'github-package-registry-npm-release' === WORKFLOW_TYPE ) {
+	if ( repo_has_file( 'package.json' ) ) {
+		$contents = file_get_contents( repo_has_file( 'package.json' ) );
+		$contents = json_decode( $contents, true );
+		print_r( $contents, true );
+
+		if ( preg_match( '#^' . '@varunsridharan' . '#', $contents['name'] ) === 1 ) {
+
+		} else {
+			$contents['name'] = '@' . REPO_OWNER . '/' . $contents['name'];
+		}
+		print_r( $contents, true );
+
+	} else {
+		__( '🛑 Unable To Find Package.json' );
+	}
+}
