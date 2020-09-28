aaa7cac7332c9f4054ea60b5c232c5a677599eab
Varun <varunsridharan23@gmail.com>
2020-09-28 16:46:59 +0530

    F***** removed the true value in print_r

--- app/workflows/github-package-registry-npm-release.php
+++ app/workflows/github-package-registry-npm-release.php
@@ -3,14 +3,14 @@ if ( 'github-package-registry-npm-release' === WORKFLOW_TYPE ) {
 	if ( repo_has_file( 'package.json' ) ) {
 		$contents = file_get_contents( repo_file( 'package.json' ) );
 		$contents = json_decode( $contents, true );
-		print_r( $contents, true );
+		print_r( $contents );
 
 		if ( preg_match( '#^' . '@varunsridharan' . '#', $contents['name'] ) === 1 ) {
 
 		} else {
 			$contents['name'] = '@' . REPO_OWNER . '/' . $contents['name'];
 		}
-		print_r( $contents, true );
+		print_r( $contents );
 
 	} else {
 		__( '🛑 Unable To Find Package.json' );
