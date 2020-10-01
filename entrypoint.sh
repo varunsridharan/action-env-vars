d5a4ee47c2661a3cfbde512a65fd8cc35fa036a4
Varun <varunsridharan23@gmail.com>
2020-10-01 08:14:35 +0530

    Install PHPExt Only if current workflow is twitter-post.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 WORKFLOW_TYPE="$INPUT_VS_WORKFLOW_TYPE"
 
-if [ "$VS_WORKFLOW_TYPE" == "twitters-post" ]; then
+if [ "$VS_WORKFLOW_TYPE" == "twitter-post" ]; then
   /usr/local/bin/install-php-extensions intl mbstring
 fi
 
